8c9552b12dd3b951f40598244565b3eb7e70b84d
thura <aung.thura@securelinkmm.com>
2021-01-05 15:43:15 +0630

    update video create and edit

--- resources/views/admin/video/edit.blade.php
+++ resources/views/admin/video/edit.blade.php
@@ -50,13 +50,16 @@
                         </div>
 
                         <div class="form-group col-sm-6 mmtext">
-                            {!! Form::label('url', 'URL:') !!} <span class="text-danger">*</span>
-                            {!! Form::text('url', null, ['class' => 'form-control']) !!}
+                            {!! Form::label('url', 'Video URL:') !!} <span class="text-danger">*</span>
+                            {{ Form::hidden('media_path', VIDEO_UPLOAD) }}
+                            <div class="file-loading">
+                                <input type="file" id="video_media" name="video" accept="*">
+                            </div>
                             @if ($errors->has('url'))
                                 <span class="text-danger">
                                     <strong>{{ $errors->first('url') }}</strong>
                                 </span>
-                            @endif
+                           @endif
                         </div>
 
                         <div class="form-group col-sm-12">
@@ -110,6 +113,7 @@
         });
          $("#video_media").fileinput({
                 overwriteInitial: true,
+                maxFileSize: 1500,
                 showClose: false,
                 showCaption: true,
                 showUpload: false,
